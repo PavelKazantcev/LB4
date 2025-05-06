5577e6bd13c15434bce3e92664a3e6797b570d8c
Владимир Ерофеев <evn-2006@mail.ru>
2025-05-06 09:32:43 +0500

    Добавлены формы для работы со справочником Типы аниме

---
+++ LB4/FormAddType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LB4
+{
+    public partial class FormAddType : Form
+    {
+        public FormAddType()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
